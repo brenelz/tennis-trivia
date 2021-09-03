a7fa3974564701c0abcb5d9215bddbbd846662b3
Brenelz <brenleydueck@gmail.com>
2021-09-02 21:11:29 -0500

    Starting point

--- pages/api/newGame.ts
+++ pages/api/newGame.ts
@@ -1,8 +1,3 @@
-import { NextApiRequest, NextApiResponse } from "next"
-import { top100Players } from "../../lib/players";
-
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  const randomizedPlayers = top100Players.sort((a, b) => 0.5 - Math.random());
-  const top5Players = randomizedPlayers.slice(0, 5);
-  res.status(200).json({players: top5Players});
+export default (req, res) => {
+  
 }
